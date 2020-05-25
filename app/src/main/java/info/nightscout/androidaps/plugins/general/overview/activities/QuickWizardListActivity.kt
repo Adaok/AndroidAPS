8e76872f80d68159070844fbca5bde5f7d8da3ba
Marco Loiodice <mloiodice13@gmail.com>
2020-05-25 23:28:07 +0200

    WIP Add function for take wizard

--- app/src/main/java/info/nightscout/androidaps/plugins/general/overview/activities/QuickWizardListActivity.kt
+++ app/src/main/java/info/nightscout/androidaps/plugins/general/overview/activities/QuickWizardListActivity.kt
@@ -1,30 +1,43 @@
 package info.nightscout.androidaps.plugins.general.overview.activities
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import info.nightscout.androidaps.MainApp
 import info.nightscout.androidaps.R
 import info.nightscout.androidaps.activities.NoSplashAppCompatActivity
 import info.nightscout.androidaps.data.QuickWizard
+import info.nightscout.androidaps.data.QuickWizard.getActive
+import info.nightscout.androidaps.data.QuickWizardEntry
+import info.nightscout.androidaps.db.DatabaseHelper
+import info.nightscout.androidaps.interfaces.Constraint
 import info.nightscout.androidaps.plugins.bus.RxBus
+import info.nightscout.androidaps.plugins.configBuilder.ConfigBuilderPlugin
+import info.nightscout.androidaps.plugins.configBuilder.ProfileFunctions
 import info.nightscout.androidaps.plugins.general.overview.dialogs.EditQuickWizardDialog
 import info.nightscout.androidaps.plugins.general.overview.events.EventQuickWizardChange
 import info.nightscout.androidaps.utils.DateUtil
 import info.nightscout.androidaps.utils.DecimalFormatter
 import info.nightscout.androidaps.utils.FabricPrivacy
+import info.nightscout.androidaps.utils.OKDialog.show
 import info.nightscout.androidaps.utils.plusAssign
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.overview_quickwizardlist_activity.*
 
 class QuickWizardListActivity : NoSplashAppCompatActivity() {
+
+    private val TAG: String = "QuickWizardListActivity"
 
     private var disposable: CompositeDisposable = CompositeDisposable()
 
@@ -48,14 +61,18 @@ class QuickWizardListActivity : NoSplashAppCompatActivity() {
             val carbs: TextView = itemView.findViewById(R.id.overview_quickwizard_item_carbs)
             val from: TextView = itemView.findViewById(R.id.overview_quickwizard_item_from)
             val to: TextView = itemView.findViewById(R.id.overview_quickwizard_item_to)
+
             //TODO change name button variable
             private val takeItButton: Button = itemView.findViewById(R.id.overview_quickwizard_item_take_button)
             private val editButton: Button = itemView.findViewById(R.id.overview_quickwizard_item_edit_button)
             private val removeButton: ImageView = itemView.findViewById(R.id.overview_quickwizard_item_remove_btn)
 
             init {
-                takeItButton.setOnClickListener{
+                takeItButton.setOnClickListener {
+                    //onClickQuickwizard(baseContext, null)
                     //TODO prepare dialog
+                    val myWizard = QuickWizard[adapterPosition]
+                    onClickQuickwizard(baseContext, myWizard)
                 }
                 editButton.setOnClickListener {
                     val manager = fragmentManager
@@ -89,18 +106,55 @@ class QuickWizardListActivity : NoSplashAppCompatActivity() {
     override fun onResume() {
         super.onResume()
         disposable += RxBus
-                .toObservable(EventQuickWizardChange::class.java)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({
-                    val adapter = RecyclerViewAdapter(supportFragmentManager)
-                    overview_quickwizardactivity_recyclerview?.swapAdapter(adapter, false)
-                }, {
-                    FabricPrivacy.logException(it)
-                })
+            .toObservable(EventQuickWizardChange::class.java)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                val adapter = RecyclerViewAdapter(supportFragmentManager)
+                overview_quickwizardactivity_recyclerview?.swapAdapter(adapter, false)
+            }, {
+                FabricPrivacy.logException(it)
+            })
     }
 
     override fun onPause() {
         disposable.clear()
         super.onPause()
     }
+
+    fun onClickQuickwizard(context: Context, quickWizardEntry: QuickWizardEntry) {
+        val actualBg = DatabaseHelper.actualBg() //is null also,
+        val profile = ProfileFunctions.getInstance().profile
+        val profileName = ProfileFunctions.getInstance().profileName
+        val pump = ConfigBuilderPlugin.getPlugin().activePump
+        //val quickWizardEntry = getActive() //change this by position quickWizard card
+
+        if (actualBg != null && profile != null && pump != null) {
+            //quickWizardButton.setVisibility(View.VISIBLE)
+            val wizard = quickWizardEntry.doCalc(profile, profileName, actualBg, true)
+
+            if (wizard.calculatedTotalInsulin > 0.0 && quickWizardEntry.carbs() > 0.0) {
+                val carbsAfterConstraints = MainApp.getConstraintChecker().applyCarbsConstraints(Constraint(quickWizardEntry.carbs())).value()
+
+                if (Math.abs(wizard.insulinAfterConstraints - wizard.calculatedTotalInsulin) >= pump.pumpDescription.pumpType.determineCorrectBolusStepSize(wizard.insulinAfterConstraints)
+                    || carbsAfterConstraints != quickWizardEntry.carbs()) {
+                        show(context, MainApp.gs(R.string.treatmentdeliveryerror), """
+                        ${MainApp.gs(R.string.constraints_violation)}
+                        ${MainApp.gs(R.string.changeyourinput)}
+                        """.trimIndent())
+                    return
+                } else {
+                    Log.d(TAG, "not in math")
+                    //todo create real log here.
+                    return
+                }
+                wizard.confirmAndExecute(context)
+            } else {
+                Log.d(TAG, "not in wizard")
+            }
+        } else {
+            Log.d(TAG, "not in quickWizard")
+        }
+    }
+
+    //region interface
 }
